bbbc2e48c000eb9722d446a432f7e11a549effd3
Niccolo' Granieri <granieriniccolo@gmail.com>
2018-06-07 18:07:27 +0100

    Version Bump
    Versionand Name Bump

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -34,8 +34,8 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "ReachCpp";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  projectName    = "Reach";
+    const char* const  versionString  = "0.1.1";
+    const int          versionNumber  = 0x101;
 }
 #endif
