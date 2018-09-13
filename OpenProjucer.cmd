104195523bf80ef2601f8641a6f1e68a4927eee6
Jefferson Bledsoe <jlbledsoe@hotmail.co.uk>
2018-09-13 14:21:11 +0100

    Update script to close command prompt after execution

--- OpenProjucer.cmd
+++ OpenProjucer.cmd
@@ -1,4 +1,5 @@
 PUSHD JUCE\extras\Projucer\Builds\VisualStudio2017
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Projucer.sln /p:Configuration=Release
-x64\Release\App\Projucer.exe
+start x64\Release\App\Projucer.exe
 POPD
+exit
