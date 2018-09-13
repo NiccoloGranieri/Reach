388417114b04078157d72299fa7d990f77bad743
Jefferson Bledsoe <jlbledsoe@hotmail.co.uk>
2018-09-13 14:10:03 +0100

    Add script to open correct Projucer version for the bundled JUCE submodule

---
+++ OpenProjucer.cmd
@@ -0,0 +1,4 @@
+PUSHD JUCE\extras\Projucer\Builds\VisualStudio2017
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Projucer.sln /p:Configuration=Release
+x64\Release\App\Projucer.exe
+POPD
