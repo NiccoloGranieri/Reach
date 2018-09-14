b8612822ec140b8027618204ba89965cfcbe2f97
Jefferson Bledsoe <jlbledsoe@hotmail.co.uk>
2018-09-14 14:32:53 +0100

    Extract logger setup into a meber function

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -33,22 +33,8 @@ public:
         LookAndFeel::setDefaultLookAndFeel (&LAF);
 
 		mainWindow = std::make_unique<MainWindow>("Reach");
-		const auto logFilename = String(getApplicationName() + "_Log_" + Time::getCurrentTime().formatted("%d-%m-%Y_%H-%M-%S"));
-		const auto logExtension = String(".csv");
 
-		auto logFile = File::getSpecialLocation (File::SpecialLocationType::userDesktopDirectory)
-				                       .getChildFile(logFilename + logExtension);
-        
-		logger = std::make_unique<LeapLogger> (logFile);
-
-		Logger::setCurrentLogger(&logger->getLogger());
-
-		logger->getLogger().logMessage(ProjectInfo::projectName + String(" Data Log"));
-		logger->getLogger().logMessage({});
-		logger->getLogger().logMessage(dataNames);
-		logger->getLogger().logMessage({});
-
-		logger->startLogger();
+		setupLogger();
     }
 
     void shutdown() override
@@ -101,6 +87,26 @@ private:
     ReachLookAndFeel LAF;
 
 	String dataNames{ "Hand,PalmX,PalmY,PalmZ,WristX,WristY,WristZ,Finger1Bone1X,Finger1Bone1Y,Finger1Bone1Z,Finger1Bone2X,Finger1Bone2Y,Finger1Bone2Z,Finger1Bone3X,Finger1Bone3Y,Finger1Bone3Z,Finger1Bone4X,Finger1Bone4Y,Finger1Bone4Z,Finger2Bone1X,Finger2Bone1Y,Finger2Bone1Z,Finger2Bone2X,Finger2Bone2Y,Finger2Bone2Z,Finger2Bone3X,Finger2Bone3Y,Finger2Bone3Z,Finger2Bone4X,Finger2Bone4Y,Finger2Bone4Z,Finger3Bone1X,Finger3Bone1Y,Finger3Bone1Z,Finger3Bone2X,Finger3Bone2Y,Finger3Bone2Z,Finger3Bone3X,Finger3Bone3Y,Finger3Bone3Z,Finger3Bone4X,Finger3Bone4Y,Finger3Bone4Z,Finger4Bone1X,Finger4Bone1Y,Finger4Bone1Z,Finger4Bone2X,Finger4Bone2Y,Finger4Bone2Z,Finger4Bone3X,Finger4Bone3Y,Finger4Bone3Z,Finger4Bone4X,Finger4Bone4Y,Finger4Bone4Z" };
+
+	void setupLogger()
+    {
+		const auto logFilename = String(getApplicationName() + "_Log_" + Time::getCurrentTime().formatted("%d-%m-%Y_%H-%M-%S"));
+		const auto logExtension = String(".csv");
+
+		auto logFile = File::getSpecialLocation(File::SpecialLocationType::userDesktopDirectory)
+			.getChildFile(logFilename + logExtension);
+
+		logger = std::make_unique<LeapLogger>(logFile);
+
+		Logger::setCurrentLogger(&logger->getLogger());
+
+		logger->getLogger().logMessage(ProjectInfo::projectName + String(" Data Log"));
+		logger->getLogger().logMessage({});
+		logger->getLogger().logMessage(dataNames);
+		logger->getLogger().logMessage({});
+
+		logger->startLogger();
+	}
 };
 
 //==============================================================================
