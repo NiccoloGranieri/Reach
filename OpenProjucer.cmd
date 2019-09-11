39c4519136bedb7bca825250a4ed6bb9a38aa038
Niccolo Granieri <granieriniccolo@gmail.com>
2019-09-11 13:49:21 +0100

    Updated Projucer Generator Script

--- OpenProjucer.cmd
+++ OpenProjucer.cmd
@@ -1,5 +1,5 @@
-PUSHD JUCE\extras\Projucer\Builds\VisualStudio2017
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Projucer.sln /p:Configuration=Release -DJUCER_ENABLE_GPL_MODE=1
+PUSHD JUCE\extras\Projucer\Builds\VisualStudio2019
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" Projucer.sln /p:Configuration=Release /p:JUCER_ENABLE_GPL_MODE=1
 start x64\Release\App\Projucer.exe
 POPD
 exit
